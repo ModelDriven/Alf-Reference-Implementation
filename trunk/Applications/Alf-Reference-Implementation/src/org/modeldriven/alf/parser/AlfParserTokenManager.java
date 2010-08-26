b2d79358f4d7693931134cea785f2d7fccdaf35e
ed-s <ed-s@ea6537cf-e006-0410-91e3-b5ef31be2fb9>
2010-08-26 23:24:57 +0000

    Rebuilt parser using latest version of JavaCC, v5.0.

--- trunk/Applications/Alf-Reference-Implementation/src/org/modeldriven/alf/parser/AlfParserTokenManager.java
+++ trunk/Applications/Alf-Reference-Implementation/src/org/modeldriven/alf/parser/AlfParserTokenManager.java
@@ -4161,8 +4161,8 @@ static final long[] jjtoMore = {
 protected SimpleCharStream input_stream;
 private final int[] jjrounds = new int[38];
 private final int[] jjstateSet = new int[76];
-private final StringBuffer jjimage = new StringBuffer();
-private StringBuffer image = jjimage;
+private final StringBuilder jjimage = new StringBuilder();
+private StringBuilder image = jjimage;
 private int jjimageLen;
 private int lengthOfMatch;
 protected char curChar;
