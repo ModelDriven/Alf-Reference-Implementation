e0c5aab8aeba056c2ec2f7121094a524c7dc55a2
ed-s <ed-s@ea6537cf-e006-0410-91e3-b5ef31be2fb9>
2011-12-23 16:17:06 +0000

    Implemented sequence operation expressions.

--- trunk/Applications/Alf-Reference-Implementation/src/org/modeldriven/alf/mapping/fuml/expressions/SequenceAccessExpressionMapping.java
+++ trunk/Applications/Alf-Reference-Implementation/src/org/modeldriven/alf/mapping/fuml/expressions/SequenceAccessExpressionMapping.java
@@ -14,11 +14,24 @@ import org.modeldriven.alf.syntax.expressions.SequenceAccessExpression;
 
 public class SequenceAccessExpressionMapping extends BehaviorInvocationExpressionMapping {
     
+    /*
+     * A sequence access expression is mapped to a call to the primitive
+     * behavior Alf::Library::PrimitiveBehaviors::SequenceFunctions::At. The
+     * result source element of the primary expression of the sequence access
+     * expression is connected by an object flow to the first argument input pin
+     * of the call behavior action. The result source element of the index
+     * expression is connected by an object flow to the second argument input
+     * pin. The result output pin of the call behavior action is the result
+     * source element for the sequence access expression.
+     */
+    
     InvocationExpression invocation = null;
     
     @Override
     public InvocationExpression getInvocationExpression() {
         if (this.invocation == null) {
+            // The getInvocation operation returns the equivalent At function
+            // invocation for the sequence access expression.
             this.invocation = 
                 this.getSequenceAccessExpression().getImpl().getInvocation();
         }
--- trunk/Applications/Alf-Reference-Implementation/src/org/modeldriven/alf/mapping/fuml/expressions/SequenceOperationExpressionMapping.java
+++ trunk/Applications/Alf-Reference-Implementation/src/org/modeldriven/alf/mapping/fuml/expressions/SequenceOperationExpressionMapping.java
@@ -11,15 +11,33 @@ package org.modeldriven.alf.mapping.fuml.expressions;
 
 import org.modeldriven.alf.mapping.fuml.expressions.InvocationExpressionMapping;
 
+import org.modeldriven.alf.syntax.expressions.BehaviorInvocationExpression;
+import org.modeldriven.alf.syntax.expressions.InvocationExpression;
 import org.modeldriven.alf.syntax.expressions.SequenceOperationExpression;
 
 public class SequenceOperationExpressionMapping extends
 		InvocationExpressionMapping {
+    
+    /**
+     * A sequence operation expression is mapped as a behavior invocation
+     * expression for the referent behavior, with the target primary expression
+     * as the first behavior argument. The result source element for the
+     * sequence operation expression is that of the behavior invocation
+     * expression.
+     */
+    
+    BehaviorInvocationExpression invocation = null;
 
-	public SequenceOperationExpressionMapping() {
-		this
-				.setErrorMessage("SequenceOperationExpressionMapping not yet implemented.");
-	}
+    @Override
+    public InvocationExpression getInvocationExpression() {
+        if (this.invocation == null) {
+            // The getInvocation operation returns the equivalent behavior
+            // invocation expression for the sequence operation expression.
+            this.invocation = 
+                this.getSequenceOperationExpression().getImpl().getInvocation();
+        }
+        return this.invocation;        
+    }
 
 	public SequenceOperationExpression getSequenceOperationExpression() {
 		return (SequenceOperationExpression) this.getSource();
