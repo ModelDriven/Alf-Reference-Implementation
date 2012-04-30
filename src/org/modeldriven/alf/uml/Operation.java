c1d3add8519748c9f74571904ea84883073c7e5f
Ed Seidewitz <ed-s@modeldriven.com>
2012-04-30 22:40:49 +0000

    Provided an interface to allow checking for external constructors and destructors.

--- src/org/modeldriven/alf/uml/Operation.java
+++ src/org/modeldriven/alf/uml/Operation.java
@@ -5,5 +5,10 @@ public interface Operation extends BehavioralFeature {
     public Type getType();
     public Integer getLower();
     public Integer getUpper();
+    
+    // NOTE: These operations are necessary because there is no standard way
+    // in the UML abstract syntax to access stereotype applications.
+    public boolean isConstructor(); // i.e, has <<Create>> stereotype applied
+    public boolean isDestructor(); // i.e., has <<Destroy>> stereotype applied
 
 }
