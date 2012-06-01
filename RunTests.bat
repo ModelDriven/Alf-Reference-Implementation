2fe5fd5763e9b58352b836f543b87905e5ac3ad6
Ed Seidewitz <ed-s@modeldriven.com>
2012-06-01 22:34:51 +0000

    Adjusted wording of licensing header.

--- RunTests.bat
+++ RunTests.bat
@@ -1,3 +1,11 @@
+@rem ***************************************************************************
+@rem Copyright 2011, 2012 Data Access Technologies, Inc. (Model Driven Solutions)
+@rem All rights reserved worldwide. This program and the accompanying materials
+@rem are made available for under the terms of the GNU General Public License 
+@rem (GPL) version 3 that accompanies this distribution and is available at 
+@rem http://www.gnu.org/licenses/gpl-3.0.html. For alternative licensing terms, 
+@rem contact Model Driven Solutions.
+@rem ***************************************************************************
 ECHO ON
 
 java -cp bin org.modeldriven.alf.parser.AlfParser tests\Example1a.alf
