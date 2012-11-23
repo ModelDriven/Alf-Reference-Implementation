c6a0673def70fb2b999f1a2273156878ac4558da
ed-s <ed-s@ea6537cf-e006-0410-91e3-b5ef31be2fb9>
2012-11-23 20:33:19 +0000

    Corrected handling of mapping of bodies of subunits.

--- trunk/Applications/Alf-Reference-Implementation/src/org/modeldriven/alf/fuml/mapping/units/NamespaceDefinitionMapping.java
+++ trunk/Applications/Alf-Reference-Implementation/src/org/modeldriven/alf/fuml/mapping/units/NamespaceDefinitionMapping.java
@@ -99,6 +99,9 @@ public abstract class NamespaceDefinitionMapping extends MemberMapping {
         // may get added to a namespace due to derivations while mapping.
         for (int i = 0; i < definition.getOwnedMember().size(); i++) {
             Member member = definition.getOwnedMember().get(i);
+            if (member.getIsStub()) {
+                member = member.getSubunit().getDefinition();
+            }
             Mapping mapping = member.getImpl().getMapping();
             if (mapping instanceof MemberMapping) {
                 ((MemberMapping)mapping).mapBody();
