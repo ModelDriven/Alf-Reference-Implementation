4751527f83148c53b99afc72c06f7eae12b533cd
ed-s <ed-s@ea6537cf-e006-0410-91e3-b5ef31be2fb9>
2013-03-10 05:28:38 +0000

    Refactored the handling of primitive behaviors to avoid the need to create a locus when only compiling not executing.

---
+++ trunk/Applications/Alf-Reference-Implementation/org.modeldriven.alf.eclipse/src/org/modeldriven/alf/eclipse/fuml/papyrus/library/common/Status.java
@@ -0,0 +1,99 @@
+/*******************************************************************************
+ * Copyright 2011, 2012 Data Access Technologies, Inc. (Model Driven Solutions)
+ * All rights reserved worldwide. This program and the accompanying materials
+ * are made available for use under the terms of the GNU General Public License 
+ * (GPL) version 3 that accompanies this distribution and is available at 
+ * http://www.gnu.org/licenses/gpl-3.0.html. For alternative licensing terms, 
+ * contact Model Driven Solutions.
+ *******************************************************************************/
+package org.modeldriven.alf.eclipse.fuml.papyrus.library.common;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.eclipse.papyrus.moka.fuml.Semantics.Classes.Kernel.DataValue;
+import org.eclipse.papyrus.moka.fuml.Semantics.Classes.Kernel.IntegerValue;
+import org.eclipse.papyrus.moka.fuml.Semantics.Classes.Kernel.PrimitiveValue;
+import org.eclipse.papyrus.moka.fuml.Semantics.Classes.Kernel.StringValue;
+import org.eclipse.papyrus.moka.fuml.Semantics.Classes.Kernel.Value;
+import org.eclipse.papyrus.moka.fuml.Semantics.Loci.LociL1.Locus;
+import org.eclipse.uml2.uml.DataType;
+import org.eclipse.uml2.uml.PrimitiveType;
+import org.eclipse.uml2.uml.Property;
+
+public class Status {
+	
+	private PrimitiveType stringType = null;
+	private PrimitiveType integerType = null;
+
+	private String context;
+	private int code;
+	private String description;
+	
+	static private DataType statusType = null;
+	
+	static public DataType getStatusType() {
+	    /*
+		if (statusType == null) {
+			statusType = (DataType)Library.getInstance().lookup("Common-Status");
+		}
+		*/
+		return statusType;
+	}
+	
+	static public void setStatusType(DataType type) {
+		statusType = type;
+	}
+	
+	public Status(Locus locus, String context, int code, String description) {
+		this.setPrimitiveTypes(locus);
+		this.setStatus(context, code, description);
+	}
+	
+	public Status(Locus locus, String context) {
+		this.setPrimitiveTypes(locus);
+		this.setStatus(context, 0, "Normal");
+	}
+	
+	private void setPrimitiveTypes(Locus locus) {
+		this.stringType = locus.factory.getBuiltInType("String");
+		this.integerType = locus.factory.getBuiltInType("Integer");
+	}
+	
+	public boolean isNormal() {
+		return this.code == 0;
+	}
+	
+	public void setStatus(String context, int code, String description) {
+		this.context = context;
+		this.code = code;
+		this.description = description;
+	}
+	
+	public Value getValue() {
+		DataValue value = new DataValue();
+		DataType statusType = getStatusType();
+		value.type = statusType;
+		for (Property attribute: statusType.getOwnedAttributes()) {
+			PrimitiveValue attributeValue = null;
+			if (attribute.getName().equals("context")) {
+				attributeValue = new StringValue();
+				attributeValue.type = this.stringType;
+				((StringValue)attributeValue).value = this.context;
+			} else if (attribute.getName().equals("code")) {
+				attributeValue = new IntegerValue();
+				attributeValue.type = this.integerType;
+				((IntegerValue)attributeValue).value = this.code;
+			} else if (attribute.getName().equals("description")) {
+				attributeValue = new StringValue();
+				attributeValue.type = this.stringType;
+				((StringValue)attributeValue).value = this.description;
+			}
+			List<Value> values = new ArrayList<Value>();
+			values.add(attributeValue);
+			value.setFeatureValue(attribute, values, 0);
+		}
+		return value;
+	}
+	
+}
---
+++ trunk/Applications/Alf-Reference-Implementation/org.modeldriven.alf.eclipse/src/org/modeldriven/alf/eclipse/fuml/papyrus/library/unlimitednaturalfunctions/UnlimitedNaturalToStringFunctionBehaviorExecution.java
@@ -0,0 +1,49 @@
+/*******************************************************************************
+ * Copyright 2011, 2012 Data Access Technologies, Inc. (Model Driven Solutions)
+ * All rights reserved worldwide. This program and the accompanying materials
+ * are made available for use under the terms of the GNU General Public License 
+ * (GPL) version 3 that accompanies this distribution and is available at 
+ * http://www.gnu.org/licenses/gpl-3.0.html. For alternative licensing terms, 
+ * contact Model Driven Solutions.
+ *******************************************************************************/
+
+package org.modeldriven.alf.eclipse.fuml.papyrus.library.unlimitednaturalfunctions;
+
+import java.util.List;
+
+import org.modeldriven.alf.eclipse.fuml.papyrus.library.LibraryFunctions;
+
+import org.eclipse.papyrus.moka.fuml.Semantics.Classes.Kernel.StringValue;
+import org.eclipse.papyrus.moka.fuml.Semantics.Classes.Kernel.UnlimitedNaturalValue;
+import org.eclipse.papyrus.moka.fuml.Semantics.Classes.Kernel.Value;
+import org.eclipse.papyrus.moka.fuml.Semantics.CommonBehaviors.BasicBehaviors.OpaqueBehaviorExecution;
+import org.eclipse.papyrus.moka.fuml.Semantics.CommonBehaviors.BasicBehaviors.ParameterValue;
+import org.eclipse.papyrus.moka.fuml.debug.Debug;
+
+public class UnlimitedNaturalToStringFunctionBehaviorExecution extends
+		OpaqueBehaviorExecution {
+
+    public void doBody(
+            List<ParameterValue> inputParameters,
+            List<ParameterValue> outputParameters) {
+
+		UnlimitedNaturalValue unv1 = (UnlimitedNaturalValue) inputParameters.get(0).values.get(0);
+		Debug.println("[doBody] argument = " + unv1.value);
+		
+		// Perform the toString operation.  If value is -1, return "*"
+		StringValue resultObj = new StringValue();
+		resultObj.value = unv1.toString();
+		resultObj.type = this.locus.factory.getBuiltInType("String");
+
+		Debug.println("[doBody] Unlimited Natural ToString result = " + resultObj.value);
+				
+		// Add output to the outputParameters list
+		LibraryFunctions.addValueToOutputList(resultObj, outputParameters);
+	}
+	
+    @Override
+    public Value new_() {
+        return new UnlimitedNaturalToStringFunctionBehaviorExecution();
+    }	
+
+} // UnlimitedNaturalToStringFunctionBehaviorExecution
