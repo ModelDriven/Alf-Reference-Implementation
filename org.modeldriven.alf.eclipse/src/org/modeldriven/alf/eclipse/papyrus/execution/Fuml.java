f23029fb41d564c0ef0659e5d63217b06b273d33
Ed Seidewitz <ed-s@modeldriven.com>
2013-03-13 23:25:35 +0000

    - Moved .library.uml files to a Libraries subdirectory. - Refactored Fuml to provide a more useful API.

--- org.modeldriven.alf.eclipse/src/org/modeldriven/alf/eclipse/papyrus/execution/Fuml.java
+++ org.modeldriven.alf.eclipse/src/org/modeldriven/alf/eclipse/papyrus/execution/Fuml.java
@@ -1,6 +1,7 @@
 package org.modeldriven.alf.eclipse.papyrus.execution;
 
 import java.util.Collection;
+import java.util.Map;
 
 import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
@@ -8,11 +9,11 @@ import org.apache.log4j.PropertyConfigurator;
 
 import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.ecore.resource.Resource;
+import org.eclipse.emf.ecore.resource.ResourceSet;
 
 import org.eclipse.papyrus.moka.fuml.Semantics.Classes.Kernel.RedefinitionBasedDispatchStrategy;
 import org.eclipse.papyrus.moka.fuml.Semantics.CommonBehaviors.Communications.FIFOGetNextEventStrategy;
 import org.eclipse.papyrus.moka.fuml.Semantics.Loci.LociL1.FirstChoiceStrategy;
-import org.eclipse.papyrus.uml.tools.utils.NameResolutionUtils;
 
 import org.eclipse.uml2.uml.resource.UMLResource;
 import org.eclipse.uml2.uml.util.UMLUtil;
@@ -46,7 +47,6 @@ import org.modeldriven.alf.uml.PrimitiveType;
 
 public class Fuml {
 	
-	private String libraryDirectory = "Libraries";
 	private String umlDirectory = ".";
 	private boolean isVerbose = false;
 	
@@ -59,8 +59,12 @@ public class Fuml {
         logger.setLevel(level);
     }
     
-    public void setLibraryDirectory(String libraryDirectory) {
-    	this.libraryDirectory = libraryDirectory;
+    public void setAlfLibraryDirectory(String alfLibraryDirectory) {
+    	this.rootScopeImpl.setModelDirectory(alfLibraryDirectory);
+    }
+    
+    public void setUmlLibraryDirectory(String umlLibraryDirectory) {
+    	this.rootScopeImpl.setLibraryDirectory(umlLibraryDirectory);
     }
     
     public void setUmlDirectory(String umlDirectory) {
@@ -69,6 +73,7 @@ public class Fuml {
     
     public void setIsVerbose(boolean isVerbose) {
         this.isVerbose = isVerbose;
+        this.rootScopeImpl.setIsVerbose(this.isVerbose);
     }
     
     private void createLocus() {
@@ -79,31 +84,35 @@ public class Fuml {
         factory.setStrategy(new FirstChoiceStrategy());       
     }
     
-    private void addPrimitiveTypes(Element scope) {
-    	Package primitiveTypes = this.getPackage(
-    			scope, "Alf::Library::PrimitiveTypes");
-    	for (NamedElement element: primitiveTypes.getMember()) {
-    		if (element instanceof PrimitiveType) {
-    			this.locus.getFactory().addBuiltInType((PrimitiveType)element);
-    			this.printVerbose("Added " + element.getQualifiedName());
-    		}
+    private void addPrimitiveTypes(ResourceSet resourceSet) {
+    	try {
+	    	Package primitiveTypes = this.getPackage(
+	    			"Alf::Library::PrimitiveTypes");
+	    	for (NamedElement element: primitiveTypes.getMember()) {
+	    		if (element instanceof PrimitiveType) {
+	    			this.locus.getFactory().addBuiltInType((PrimitiveType)element);
+	    			this.printVerbose("Added " + element.getQualifiedName());
+	    		}
+	    	}
+    	} catch (ElementResolutionError e) {
+    		this.println(e.getMessage());
     	}
     }
     
-    private void addPrimitiveBehaviorPrototypes(Element scope) {
+    private void addPrimitiveBehaviorPrototypes(ResourceSet resourceSet) {
     	QualifiedName qualifiedName = new QualifiedName().getImpl().
     			addName("Alf").getImpl().
     			addName("Library").getImpl().
     			addName("PrimitiveBehaviors");
-    	this.createPrimitiveBehaviorPrototypes(scope, qualifiedName);
+    	this.createPrimitiveBehaviorPrototypes(resourceSet, qualifiedName);
     	
     	qualifiedName = new QualifiedName().getImpl().
     			addName("FoundationalModelLibrary").getImpl().
     			addName("PrimitiveBehaviors");
-    	this.createPrimitiveBehaviorPrototypes(scope, qualifiedName);
+    	this.createPrimitiveBehaviorPrototypes(resourceSet, qualifiedName);
     }
     
-    private void createPrimitiveBehaviorPrototypes(Element scope, QualifiedName qualifiedName) {
+    private void createPrimitiveBehaviorPrototypes(ResourceSet resourceSet, QualifiedName qualifiedName) {
     	String pathName = qualifiedName.getImpl().getPathName();
     	UnitDefinition unit = this.rootScopeImpl.resolveUnit(qualifiedName);
     	NamespaceDefinition definition = unit.getDefinition();
@@ -117,19 +126,22 @@ public class Fuml {
 		    			String packageName = pathName + "::" + packageDefinition.getName();
 		    			for (Member member: ((PackageDefinition) packageDefinition).getOwnedMember()) {
 		    				if (member instanceof ActivityDefinition && member.getIsPrimitive()) {
-		    					Classifier behavior = this.getClassifier(
-		    							scope, 
-		    							packageName + "::" + member.getName());
-		    					if (behavior instanceof OpaqueBehavior) {
-		    						OpaqueBehaviorExecution execution = 
-		    								instantiatePrimitiveBehaviorPrototype(
-		    										(ActivityDefinition)member, 
-		    										(OpaqueBehavior)behavior);
-		    						if (execution != null) {
-		    							this.locus.getFactory().addPrimitiveBehaviorPrototype(execution);
-		    							this.printVerbose("Added " + behavior.getQualifiedName());
-		    						}
-		    					}
+		    					try {
+			    					Classifier behavior = this.getClassifier(
+			    							packageName + "::" + member.getName());
+			    					if (behavior instanceof OpaqueBehavior) {
+			    						OpaqueBehaviorExecution execution = 
+			    								instantiatePrimitiveBehaviorPrototype(
+			    										(ActivityDefinition)member, 
+			    										(OpaqueBehavior)behavior);
+			    						if (execution != null) {
+			    							this.locus.getFactory().addPrimitiveBehaviorPrototype(execution);
+			    							this.printVerbose("Added " + behavior.getQualifiedName());
+			    						}
+			    					}
+		    			    	} catch (ElementResolutionError e) {
+		    			    		this.println(e.getMessage());
+		    			    	}
 		    				}
 		    			}
 		    		}
@@ -152,25 +164,38 @@ public class Fuml {
         return execution;
     }
     
-    protected void createSystemServices(Element scope) {
+    protected void createSystemServices(ResourceSet resourceSet) {
     	String basicInputOutput = "Alf::Library::BasicInputOutput";
-    	Classifier standardOutputChannel = this.getClassifier(
-    			scope, basicInputOutput + "::StandardOutputChannel");
-        this.createSystemService
-            (standardOutputChannel, new StandardOutputChannelObject());
+    	
+    	try {
+	    	Classifier standardOutputChannel = this.getClassifier(
+	    			basicInputOutput + "::StandardOutputChannel");
+	        this.createSystemService
+	            (standardOutputChannel, new StandardOutputChannelObject());
+    	} catch (ElementResolutionError e) {
+    		this.println(e.getMessage());
+    	}
         
-        Classifier standardInputChannel = this.getClassifier(
-        		scope, basicInputOutput + "::StandardInputChannel");
-        this.createSystemService
-            (standardInputChannel, new StandardInputChannelObject());
+    	try {
+	        Classifier standardInputChannel = this.getClassifier(
+	        		basicInputOutput + "::StandardInputChannel");
+	        this.createSystemService
+	            (standardInputChannel, new StandardInputChannelObject());
+    	} catch (ElementResolutionError e) {
+    		this.println(e.getMessage());
+    	}
         
-        Classifier statusType = this.getClassifier(
-        		scope, basicInputOutput + "::Status");
-        if (statusType instanceof DataType) {
-            Status.setStatusType(((org.modeldriven.alf.eclipse.uml.DataType)statusType).getBase());
-        } else {
-            System.out.println("Cannot find Status datatype.");
-        }
+    	try {
+	        Classifier statusType = this.getClassifier(
+	        		basicInputOutput + "::Status");
+	        if (statusType instanceof DataType) {
+	            Status.setStatusType(((org.modeldriven.alf.eclipse.uml.DataType)statusType).getBase());
+	        } else {
+	            this.println("Cannot find Status datatype.");
+	        }
+    	} catch (ElementResolutionError e) {
+    		this.println(e.getMessage());
+    	}
     }
     
     private void createSystemService (
@@ -186,62 +211,43 @@ public class Fuml {
         }
     }
     
-    private Package getPackage(Element scope, String qualifiedName) {
-    	Element element = this.getElement(scope, qualifiedName);
+    public class ElementResolutionError extends Exception {
+
+		private static final long serialVersionUID = 1L;
+		
+		public ElementResolutionError(String message) {
+			super(message);
+		}
+    	
+    }
+    
+    public Package getPackage(String qualifiedName) throws ElementResolutionError {
+    	Element element = this.getElement(qualifiedName);
     	if (!(element instanceof Package)) {
-    		this.println(qualifiedName + " is not a Package.");
-    		return null;
+    		throw new ElementResolutionError(qualifiedName + " is not a Package.");
     	} else {
     		return (Package)element;
     	}
     }
     
-    private Classifier getClassifier(Element scope, String qualifiedName) {
-    	Element element = this.getElement(scope, qualifiedName);
+    public Classifier getClassifier(String qualifiedName) throws ElementResolutionError {
+    	Element element = this.getElement(qualifiedName);
     	if (!(element instanceof Classifier)) {
-    		this.println(qualifiedName + " is not a Classifier.");
-    		return null;
+    		throw new ElementResolutionError(qualifiedName + " is not a Classifier.");
     	} else {
     		return (Classifier)element;
     	}
     }
     
-    private Classifier getClassifier(Resource resource, String qualifiedName) {
-    	Element element = this.getElement(resource, qualifiedName);
-    	if (!(element instanceof Classifier)) {
-    		this.println(qualifiedName + " is not a Classifier.");
-    		return null;
-    	} else {
-    		return (Classifier)element;
-    	}
-    }
-    
-    private Element getElement(Element scope, String qualifiedName) {
+    public Element getElement(String qualifiedName) throws ElementResolutionError {
     	Element element = null;
     	Collection<org.eclipse.uml2.uml.NamedElement> elements = 
-    			NameResolutionUtils.getNamedElements(
-    					qualifiedName, 
-    					((org.modeldriven.alf.eclipse.uml.Element)scope).getBase(), 
-    					null);
+    			UMLUtil.findNamedElements(
+    					this.rootScopeImpl.getResourceSet(), qualifiedName);
     	if (elements.size() == 0) {
-    		this.println("Cannot find " + qualifiedName);
+    		throw new ElementResolutionError("Cannot find " + qualifiedName);
     	} else if (elements.size() > 1) {
-    		this.println("More than one " + qualifiedName);
-    	} else {
-    		element = org.modeldriven.alf.eclipse.uml.Element.
-    				wrap((org.eclipse.uml2.uml.Element)elements.toArray()[0]);
-    	}
-    	return element;    	
-    }
-    
-    private Element getElement(Resource resource, String qualifiedName) {
-    	Element element = null;
-    	Collection<org.eclipse.uml2.uml.NamedElement> elements = 
-    			UMLUtil.findNamedElements(resource, qualifiedName);
-    	if (elements.size() == 0) {
-    		this.println("Cannot find " + qualifiedName);
-    	} else if (elements.size() > 1) {
-    		this.println("More than one " + qualifiedName);
+    		throw new ElementResolutionError("More than one " + qualifiedName);
     	} else {
     		element = org.modeldriven.alf.eclipse.uml.Element.
     				wrap((org.eclipse.uml2.uml.Element)elements.toArray()[0]);
@@ -264,7 +270,7 @@ public class Fuml {
             if (i < args.length) {
                 if (option.equals("v")) {
                     this.setIsVerbose(true);
-                 } else if (option.matches("[dlu]")) {
+                 } else if (option.matches("[dLlu]")) {
                     arg = args[i];
                     if (arg.length() > 0 && arg.charAt(0) == '-') {
                         return null;
@@ -273,8 +279,10 @@ public class Fuml {
                     if (option.equals("d")) {
                     	setDebugLevel(Level.toLevel(arg, level));
                     	level = logger.getLevel();
+                    } else if (option.equals("L")) {
+                    	this.setAlfLibraryDirectory(arg);
                     } else if (option.equals("l")) {
-                    	this.setLibraryDirectory(arg);
+                    	this.setUmlLibraryDirectory(arg);
                     } else if (option.equals("u")) {
                     	this.setUmlDirectory(arg);
                     }
@@ -305,69 +313,87 @@ public class Fuml {
     	return operation;
     }
     
-    public void execute(String name) {
-    	this.rootScopeImpl.setModelDirectory(this.libraryDirectory);
-    	this.rootScopeImpl.setLibraryDirectory(this.umlDirectory);
-        this.rootScopeImpl.setIsVerbose(this.isVerbose);
-        // this.rootScopeImpl.initialize();	
-
+    public void initializeEnvironment() {
+        this.rootScopeImpl.initialize();	
+        
+        ResourceSet resourceSet = this.rootScopeImpl.getResourceSet();
+        
+    	this.createLocus();
+    	this.addPrimitiveTypes(resourceSet);
+    	this.addPrimitiveBehaviorPrototypes(resourceSet);
+    	this.createSystemServices(resourceSet);
+    }
+    
+    public Resource getResource(String name) {
     	Resource resource = 
     			this.rootScopeImpl.getResource(this.umlDirectory, name);
-    	
     	if (resource != null) {
-    		Classifier element = this.getClassifier(resource, "Model::" + name);
-    		
-    		if (element instanceof Behavior && 
-    				((Behavior)element).getOwnedParameter().isEmpty() ||
-    				element instanceof Class_ && 
-    				((Class_)element).getIsActive() && 
-    				!((Class_)element).getIsAbstract() && 
-    				((Class_)element).getClassifierBehavior() != null) {
-
-    			resource.getResourceSet().getURIConverter().getURIMap().
-    				put(URI.createURI(""), resource.getURI());
-
-    			this.createLocus();
-    			this.addPrimitiveTypes(element);
-    			this.addPrimitiveBehaviorPrototypes(element);
-    			this.createSystemServices(element);
-
-    			this.printVerbose("Executing...");
-    			if (element instanceof Behavior) {
-    				this.locus.getExecutor().execute((Behavior)element, null);
-    			} else {
-    				// Instantiate active class.
-    				Class_ class_ = (Class_)element;
-    				Object_ object = locus.instantiate(class_);
-
-    				// Initialize the object.
-    				Operation initializer = getInitializationOperation(class_);        		
-    				if (initializer != null) {
-    					this.locus.getExecutor().execute(
-    							((Behavior)initializer.getMethod().get(0)), 
-    							object);
-    				}
-
-    				// Execute the classifier behavior.
-    				object.startBehavior(class_);
-    			}
-
-    		} else if (element instanceof Behavior) {
-    			this.println("Cannot execute a behavior with parameters.");
-    		} else if (element instanceof Class_) {
-    			Class_ class_ = (Class_)element;
-    			if (!class_.getIsActive()) {
-    				this.println("Cannot execute a class that is not active.");
-    			} else if (class_.getIsAbstract()) {
-    				this.println("Cannot execute an abstract class.");
-    			} else {
-    				this.println("Cannot execute a class without a classifier behavior.");
-    			}
-    		} else {
-    			this.println("Unit not executable.");
-    		}
+			Map<URI, URI> map = resource.getResourceSet().getURIConverter().getURIMap();
+			map.put(URI.createURI(""), resource.getURI());
     	}
+    	return resource;	
     }
+    
+    public void execute(Classifier element) {
+        if (element instanceof Behavior && 
+        		((Behavior)element).getOwnedParameter().isEmpty() ||
+        		element instanceof Class_ && 
+        		((Class_)element).getIsActive() && 
+        		!((Class_)element).getIsAbstract() && 
+        		((Class_)element).getClassifierBehavior() != null) {
+
+        	this.printVerbose("Executing...");
+        	if (element instanceof Behavior) {
+        		this.locus.getExecutor().execute((Behavior)element, null);
+        	} else {
+        		// Instantiate active class.
+        		Class_ class_ = (Class_)element;
+        		Object_ object = locus.instantiate(class_);
+
+        		// Initialize the object.
+        		Operation initializer = getInitializationOperation(class_);        		
+        		if (initializer != null) {
+        			this.locus.getExecutor().execute(
+        					((Behavior)initializer.getMethod().get(0)), 
+        					object);
+        		}
+
+        		// Execute the classifier behavior.
+        		object.startBehavior(class_);
+        	}
+
+        } else if (element instanceof Behavior) {
+        	this.println("Cannot execute a behavior with parameters.");
+        } else if (element instanceof Class_) {
+        	Class_ class_ = (Class_)element;
+        	if (!class_.getIsActive()) {
+        		this.println("Cannot execute a class that is not active.");
+        	} else if (class_.getIsAbstract()) {
+        		this.println("Cannot execute an abstract class.");
+        	} else {
+        		this.println("Cannot execute a class without a classifier behavior.");
+        	}
+        } else {
+        	this.println("Unit not executable.");
+        }
+    }
+    
+   public void execute(String name) {
+	   	try {
+	      	Resource resource = this.getResource(name);
+	    	if (resource != null) {
+	    		try {
+	    			this.initializeEnvironment();
+		    		Classifier element = this.getClassifier("Model::" + name);
+		        	this.execute(element);
+	        	} catch (ElementResolutionError e) {
+	        		this.println(e.getMessage());
+	        	}
+	    	}
+	   	} catch (Exception e) {
+	   		this.println(e.getMessage());
+	   	}
+   }
     
    protected void printVerbose(String message) {
     	if (this.isVerbose) {
@@ -381,6 +407,9 @@ public class Fuml {
     
     public Fuml() {
         PropertyConfigurator.configure("log4j.properties");
+        
+        this.setAlfLibraryDirectory("../Libraries");
+        this.setUmlLibraryDirectory("Libraries");
     }
     
     public Fuml(String[] args) {
@@ -397,8 +426,8 @@ public class Fuml {
         		name = name.substring(0, l1 - l2);
         	}
         	
-        	this.execute(name);
-
+    		this.execute(name);
+ 
         } else {
         	this.println("Usage is");
         	this.println("  fuml [options] file");
@@ -406,7 +435,8 @@ public class Fuml {
         	this.println("allowable options are:");
         	this.println("  -d OFF|FATAL|ERROR|WARN|INFO|DEBUG|ALL");
         	this.println("            Set debug logging level (default is as configured)");
-        	this.println("  -l path   Set Alf library directory (default is \"Libraries\"");
+        	this.println("  -L path   Set Alf library directory (default is \"../Libraries\"");
+        	this.println("  -l path   Set UML library directory (default is \"Libraries\"");
         	this.println("  -u path   Set UML directory path (default is \".\"");
         	this.println("  -v        Set verbose mode");
         }         
