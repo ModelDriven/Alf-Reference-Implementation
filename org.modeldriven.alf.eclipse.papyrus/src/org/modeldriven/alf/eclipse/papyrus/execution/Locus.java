a3877efcccc1c329acbf8ee1bfeccdcf981e758f
Ed Seidewitz <ed-s@modeldriven.com>
2013-03-19 18:01:46 +0000

    Added getExtent method to Locus wrapper.

--- org.modeldriven.alf.eclipse.papyrus/src/org/modeldriven/alf/eclipse/papyrus/execution/Locus.java
+++ org.modeldriven.alf.eclipse.papyrus/src/org/modeldriven/alf/eclipse/papyrus/execution/Locus.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright 2011, 2012 Data Access Technologies, Inc. (Model Driven Solutions)
+ * Copyright 2011-2013 Data Access Technologies, Inc. (Model Driven Solutions)
  * All rights reserved worldwide. This program and the accompanying materials
  * are made available for use under the terms of the GNU General Public License 
  * (GPL) version 3 that accompanies this distribution and is available at 
@@ -8,53 +8,71 @@
  *******************************************************************************/
 package org.modeldriven.alf.eclipse.papyrus.execution;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.modeldriven.alf.fuml.impl.execution.Object_;
 import org.modeldriven.alf.uml.Class_;
 
 import org.eclipse.papyrus.moka.fuml.Semantics.Loci.LociL3.ExecutionFactoryL3;
 
 public class Locus implements org.modeldriven.alf.fuml.execution.Locus {
     
-    org.eclipse.papyrus.moka.fuml.Semantics.Loci.LociL1.Locus locus = null;
+    private org.eclipse.papyrus.moka.fuml.Semantics.Loci.LociL1.Locus base = null;
     
     public Locus() {
-        this.locus = new org.eclipse.papyrus.moka.fuml.Semantics.Loci.LociL1.Locus();
-        this.locus.setExecutor(new org.eclipse.papyrus.moka.fuml.Semantics.Loci.LociL1.Executor());
-        this.locus.setFactory(new org.eclipse.papyrus.moka.fuml.Semantics.Loci.LociL3.ExecutionFactoryL3());
+        this.base = new org.eclipse.papyrus.moka.fuml.Semantics.Loci.LociL1.Locus();
+        this.base.setExecutor(new org.eclipse.papyrus.moka.fuml.Semantics.Loci.LociL1.Executor());
+        this.base.setFactory(new org.eclipse.papyrus.moka.fuml.Semantics.Loci.LociL3.ExecutionFactoryL3());
     }
     
-    public Locus(org.eclipse.papyrus.moka.fuml.Semantics.Loci.LociL1.Locus locus) {
-        this.locus = locus;
+    public Locus(org.eclipse.papyrus.moka.fuml.Semantics.Loci.LociL1.Locus base) {
+        this.base = base;
+    }
+    
+    public org.eclipse.papyrus.moka.fuml.Semantics.Loci.LociL1.Locus getBase() {
+    	return this.base;
     }
     
     @Override
+    public List<Object_> getExtent(Class_ class_) {
+    	List<Object_> list = new ArrayList<Object_>();
+    	for (org.eclipse.papyrus.moka.fuml.Semantics.Classes.Kernel.ExtensionalValue object: 
+    		this.getBase().getExtent(((org.modeldriven.alf.eclipse.uml.Class_)class_).getBase())) {
+    		list.add(new Object_((org.eclipse.papyrus.moka.fuml.Semantics.Classes.Kernel.Object_)object));
+    	}
+    	return list;
+    }
+
+    @Override
     public ExecutionFactory getFactory() {
-        return this.locus.factory == null? null: new ExecutionFactory(locus.factory);
+        return this.base.factory == null? null: new ExecutionFactory(base.factory);
     }
 
     @Override
     public Executor getExecutor() {
-        return this.locus.executor == null? null: new Executor(locus.executor);
+        return this.base.executor == null? null: new Executor(base.executor);
     }
 
     @Override
     public Object_ instantiate(Class_ type) {
         org.eclipse.papyrus.moka.fuml.Semantics.Classes.Kernel.Object_ object_ = 
                 type == null? null: 
-                this.locus.instantiate(((org.modeldriven.alf.eclipse.uml.Class_)type).getBase());
+                this.base.instantiate(((org.modeldriven.alf.eclipse.uml.Class_)type).getBase());
         return object_ == null? null: new Object_(object_);
     }
 
     public void setExecutor(org.eclipse.papyrus.moka.fuml.Semantics.Loci.LociL1.Executor executor) {
-        this.locus.setExecutor(executor);
+        this.base.setExecutor(executor);
     }
 
     public void setFactory(ExecutionFactoryL3 factory) {
-        this.locus.setFactory(factory);
+        this.base.setFactory(factory);
     }
 
     public void add(org.eclipse.papyrus.moka.fuml.Semantics.Classes.Kernel.Object_ object) {
         if (object != null) {
-            this.locus.add(object);
+            this.base.add(object);
         }
     }
 
