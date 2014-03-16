0322b71e1ec596f12489189561b936c7a62a78ec
ed-s <ed-s@ea6537cf-e006-0410-91e3-b5ef31be2fb9>
2014-03-16 22:54:03 +0000

    Added copyright notice.

--- trunk/Applications/Alf-Reference-Implementation/org.modeldriven.alf.fuml.impl/src/org/modeldriven/alf/fuml/impl/execution/test/RunTests.java
+++ trunk/Applications/Alf-Reference-Implementation/org.modeldriven.alf.fuml.impl/src/org/modeldriven/alf/fuml/impl/execution/test/RunTests.java
@@ -1,3 +1,12 @@
+/*******************************************************************************
+ * Copyright 2014 Ivar Jacobson International SA
+ * 
+ * All rights reserved worldwide. This program and the accompanying materials
+ * are made available for use under the terms of the GNU General Public License 
+ * (GPL) version 3 that accompanies this distribution and is available at 
+ * http://www.gnu.org/licenses/gpl-3.0.html. 
+ *******************************************************************************/
+
 package org.modeldriven.alf.fuml.impl.execution.test;
 
 import org.modeldriven.alf.fuml.impl.execution.Alf;
