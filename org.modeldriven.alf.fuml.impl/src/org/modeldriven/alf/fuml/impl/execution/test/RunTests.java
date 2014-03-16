1121afc5cc9102aea4d0107f2fdb03c05eef25d4
Ed Seidewitz <ed-s@modeldriven.com>
2014-03-16 22:28:43 +0000

    Replaced RunTests Ant script with Java class.

---
+++ org.modeldriven.alf.fuml.impl/src/org/modeldriven/alf/fuml/impl/execution/test/RunTests.java
@@ -0,0 +1,11 @@
+package org.modeldriven.alf.fuml.impl.execution.test;
+
+import org.modeldriven.alf.fuml.impl.execution.Alf;
+
+public class RunTests extends org.modeldriven.alf.fuml.execution.test.RunTests {
+
+	public static void main(String[] args) {
+		runTests(new Alf());
+	}
+
+}
