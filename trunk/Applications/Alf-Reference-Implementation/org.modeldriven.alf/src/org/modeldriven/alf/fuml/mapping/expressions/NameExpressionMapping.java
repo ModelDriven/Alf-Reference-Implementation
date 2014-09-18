b868a9b485024fed893c5357698686fee78f73e1
ed-s <ed-s@ea6537cf-e006-0410-91e3-b5ef31be2fb9>
2014-09-18 22:53:54 +0000

    Corrected the construction of the value specification action when mapping an external enumeration literal reference.

--- trunk/Applications/Alf-Reference-Implementation/org.modeldriven.alf/src/org/modeldriven/alf/fuml/mapping/expressions/NameExpressionMapping.java
+++ trunk/Applications/Alf-Reference-Implementation/org.modeldriven.alf/src/org/modeldriven/alf/fuml/mapping/expressions/NameExpressionMapping.java
@@ -1,6 +1,6 @@
 
 /*******************************************************************************
- * Copyright 2011, 2012 Data Access Technologies, Inc. (Model Driven Solutions)
+ * Copyright 2011-2014 Data Access Technologies, Inc. (Model Driven Solutions)
  * All rights reserved worldwide. This program and the accompanying materials
  * are made available for use under the terms of the GNU General Public License 
  * (GPL) version 3 that accompanies this distribution and is available at 
@@ -109,9 +109,9 @@ public class NameExpressionMapping extends ExpressionMapping {
                         this.throwError("Error mapping enumeration literal:" + 
                                 enumerationLiteralReference);
                     }
-                    this.action = this.graph.addDataValueSpecificationAction(literal);
-                    this.activityNode = this.action.getResult();
                 }
+                this.action = this.graph.addDataValueSpecificationAction(literal);
+                this.activityNode = this.action.getResult();
             } else if (propertyAccess != null) {
                 FumlMapping mapping = this.fumlMap(propertyAccess);
                 if (mapping instanceof PropertyAccessExpressionMapping) {
