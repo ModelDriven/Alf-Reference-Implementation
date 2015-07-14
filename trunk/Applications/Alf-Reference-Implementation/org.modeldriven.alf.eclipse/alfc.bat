647b2930b86a69beb345f293c53873abac9b8772
ed-s <ed-s@ea6537cf-e006-0410-91e3-b5ef31be2fb9>
2015-07-14 17:27:12 +0000

    Added scripts for compiling and Alf unit to a UML file and for executing such a file.

---
+++ trunk/Applications/Alf-Reference-Implementation/org.modeldriven.alf.eclipse/alfc.bat
@@ -0,0 +1,14 @@
+@rem ***************************************************************************
+@rem Copyright 2015 Data Access Technologies, Inc. (Model Driven Solutions)
+@rem
+@rem All rights reserved worldwide. This program and the accompanying materials
+@rem are made available for under the terms of the GNU General Public License 
+@rem (GPL) version 3 that accompanies this distribution and is available at 
+@rem http://www.gnu.org/licenses/gpl-3.0.html.
+@rem ***************************************************************************
+@echo off
+
+set lib=./UML/Libraries
+if not "%UML_LIB%" == "" set lib=%UML_LIB%
+
+java -jar dist/alf-eclipse.jar -l %UML_LIB% %*
