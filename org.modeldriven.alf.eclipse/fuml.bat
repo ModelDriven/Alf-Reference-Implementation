fe83c8298e3026b6b23a511ac6ce671aa50edd4e
Ed Seidewitz <ed-s@modeldriven.com>
2015-07-14 17:27:12 +0000

    Added scripts for compiling and Alf unit to a UML file and for executing such a file.

---
+++ org.modeldriven.alf.eclipse/fuml.bat
@@ -0,0 +1,14 @@
+@rem ***************************************************************************
+@rem Copyright 2015 Data Access Technologies, Inc. (Model Driven Solutions)
+@rem
+@rem All rights reserved worldwide. This program and the accompanying materials
+@rem are made available for under the terms of the GNU General Public License 
+@rem (GPL) version 3 that accompanies this distribution and is available at 
+@rem http://www.gnu.org/licenses/gpl-3.0.html.
+@rem ***************************************************************************
+@echo off
+
+set lib=
+if not "%UML_LIB%" == "" set lib=-l %UML_LIB%
+
+java -jar dist/alf-eclipse.jar %UML_LIB% %*
