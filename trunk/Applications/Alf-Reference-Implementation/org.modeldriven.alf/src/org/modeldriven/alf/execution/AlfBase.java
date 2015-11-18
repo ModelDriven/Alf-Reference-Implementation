318bbf9d48dc320a92ff6a6a3d94d65bd2f95e8e
ed-s <ed-s@ea6537cf-e006-0410-91e3-b5ef31be2fb9>
2015-11-18 21:58:20 +0000

    Maintenance version 0.5.1d.

--- trunk/Applications/Alf-Reference-Implementation/org.modeldriven.alf/src/org/modeldriven/alf/execution/AlfBase.java
+++ trunk/Applications/Alf-Reference-Implementation/org.modeldriven.alf/src/org/modeldriven/alf/execution/AlfBase.java
@@ -19,7 +19,7 @@ import org.modeldriven.alf.syntax.units.UnitDefinition;
 
 public abstract class AlfBase {
     
-    public static final String ALF_VERSION = "0.5.1c";
+    public static final String ALF_VERSION = "0.5.1d";
     
     protected boolean isVerbose = false;
 
