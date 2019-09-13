908ba163858143719618ae4fcfa2bbdf9df21c8e
Ed Seidewitz <ed-s@modeldriven.com>
2019-09-12 23:54:58 -0400

    Updated for v1.1.0h.

--- org.modeldriven.alf/src/org/modeldriven/alf/execution/AlfBase.java
+++ org.modeldriven.alf/src/org/modeldriven/alf/execution/AlfBase.java
@@ -25,7 +25,7 @@ import org.modeldriven.alf.syntax.units.UnitDefinition;
 
 public abstract class AlfBase {
     
-    public static final String ALF_VERSION = "1.1.0g/maint-3";
+    public static final String ALF_VERSION = "1.1.0h";
     
     protected boolean isVerbose = false;
 
